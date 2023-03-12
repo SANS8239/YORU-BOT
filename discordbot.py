ec678bc7d8c10294f7c26d4e5d5dc0804a9d860c
SANS8239 <123286625+SANS8239@users.noreply.github.com>
2023-03-12 11:43:49 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -20,6 +20,7 @@ import os
 import subprocess
 import pytube as pt
 import glob
+from discord.utils import get
 
 ## using environment variable ##
 PREFIX = os.environ['PREFIX']
@@ -35,8 +36,45 @@ client = commands.Bot(command_prefix='/', intents = intents)
 async def on_ready():
      await client.change_presence(status=discord.Status.online, activity=discord.Game("난 바보가 아니야... 바보가 아니라고!"))
 
+@client.command(aliases = ['요루'])
+async def yoru (ctx, url) :
+     voice_channel = get(ctx.guild.voice_channels, name='음성채널이름')
+     if not voice_channel:
+        await ctx.send(embed = discord.Embed(description = "음.. 나에게 노래를 부르게 시키고 싶으면 음성채널에 먼저 접속해 줘!", color = 0x000000))
+        return
+     
+    await voice_channel.connect()
+    voice_client = get(bot.voice_clients, guild=ctx.guild)
+     
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'quiet': True,
+        'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+        'nocheckcertificate': True,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192'
+        }]
+    }
 
-           
+     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+       info_dict = ydl.extract_info(url, download=False)
+       video_title = info_dict.get('title', None)
+       video_url = info_dict.get('url', None)
+     
+     source = await discord.FFmpegOpusAudio.from_probe(video_url)
+     voice_client.play(source)
+     await ctx.send(f'**{video_title}**을(를) 재생합니다.')
+
+     
+     @client.command(aliases = ['요루정지'])
+     async def yoru_stop (ctx) :
+          voice_client = get(bot.voice_clients, guild=ctx.guild)
+          if voice_client.is_playing():
+              voice_client.stop()
+              await voice_client.disconnect()
+              await ctx.send("노래를 정지하고 음성채널에서 나갑니다.")
 
 try:
     client.run(TOKEN)
